44d22441352028d68144db23444b9e44aa8bfcac
Jákup Viljam Dam <s185095@dtu.dk>
2024-09-15 18:45:49 +0200

    notes from regex

---
+++ solutions/regex.py
@@ -0,0 +1,7 @@
+
+import sys
+
+# Class/Method name regex
+# ^([a-zA-Z_][\w\.]*)\.([a-zA-Z_][\w]*)\:\(([\w]*)\)(\w)
+# Source code for methods regex
+# "^\s*(public)?\s*(static)\s*([a-zA-Z_][\w]*)\s*([a-zA-Z_][\w]*)(\([^}]*)\s(\{[^}]*\})"gm
